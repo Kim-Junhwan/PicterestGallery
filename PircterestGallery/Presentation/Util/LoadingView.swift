7ad06770cb531eb433ea3f714fdae30b89f81be2
Kim-Junhwan <tazanboy1@naver.com>
2023-01-31 17:38:51 +0900

    [refactor] 로딩화면 hidden 상태로 변경
    - 기존 로딩화면을 uiwindow에서 제거하는 방식에서 hidden상태가 되도록 변경

--- PircterestGallery/Presentation/Util/LoadingView.swift
+++ PircterestGallery/Presentation/Util/LoadingView.swift
@@ -8,29 +8,33 @@
 import UIKit
 
 public class LoadingView {
-    
-    static var spinner: UIActivityIndicatorView?
+
+     static var spinner: UIActivityIndicatorView?
     
     static let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-    
+
     public static func show() {
-        if spinner == nil{
-            guard let windowScene = windowScene else {return}
-            let frame = windowScene.screen.bounds
-            let spinner = UIActivityIndicatorView(frame: frame)
-            spinner.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-            spinner.style = .large
-            spinner.color = .white
-            windowScene.keyWindow?.addSubview(spinner)
-            spinner.startAnimating()
-            self.spinner = spinner
-        }
+            if spinner == nil{
+                guard let windowScene = windowScene else {return}
+                let frame = windowScene.screen.bounds
+                let spinner = UIActivityIndicatorView(frame: frame)
+                spinner.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+                spinner.style = .large
+                spinner.color = .white
+                spinner.hidesWhenStopped = true
+                windowScene.keyWindow?.addSubview(spinner)
+                spinner.startAnimating()
+                self.spinner = spinner
+            } else {
+                guard let spinner = spinner else { return }
+                spinner.startAnimating()
+            }
     }
-    
+
     public static func hide() {
-        guard let spinner = spinner else { return }
-        spinner.stopAnimating()
-        spinner.removeFromSuperview()
-        self.spinner = nil
+            guard let spinner = spinner else { return }
+            spinner.stopAnimating()
+            //spinner.removeFromSuperview()
+            //self.spinner = nil
     }
 }
